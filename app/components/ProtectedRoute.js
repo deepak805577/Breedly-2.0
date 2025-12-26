6239bfcbb023f8619d38bbc36b28d86993054b13
deepak805577 <deepakg805577@gmail.com>
2025-12-26 15:12:13 +0530

    login

--- app/components/ProtectedRoute.js
+++ app/components/ProtectedRoute.js
@@ -7,13 +7,20 @@ export default function ProtectedRoute({ children }) {
   const router = useRouter();
 
   useEffect(() => {
-    const username = localStorage.getItem("username"); // or "token" if you saved that
-    if (!username) {
-      alert("Please login first!");
+    const username = localStorage.getItem("username"); // or token
+    const redirectedOnce = sessionStorage.getItem("redirectedOnce");
+
+    if (!username && !redirectedOnce) {
+      // First-time redirect to login
+       alert("You need to login to access this page. Redirecting to Home.");
+      sessionStorage.setItem("redirectedOnce", "true");
       router.push("/login");
+    } else if (!username && redirectedOnce) {
+         alert("You need to login to access this page. Redirecting to Home.");
+      // Subsequent attempts: redirect to Home
+      router.push("/");
     }
   }, [router]);
 
   return <>{children}</>; // render children if logged in
 }
-
